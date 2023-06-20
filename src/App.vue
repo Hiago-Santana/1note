995526a028557b653184bf3c4b9d02e780d6145b
Hiago-Santana <hiago_douglas@hotmail.com>
2023-06-20 13:34:00 -0300

    Enhancement

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,63 @@ import { addNote, readAllNote, deleteNote, setNote } from './components/indexedd
 const enteredTitle = ref('');
 const enteredDescription = ref('');
 const indexNote = ref([]);
-const toglleModal = ref(false);
+const toggleModal = ref(false);
 const indexNoteCopy = ref([]);
 const allNote = ref([]);
-const toglleTitle = ref(false);
+const toggleTitle = ref(false);
 const toggleSave = ref(false);
 const disabled = ref(false)
 const screenWidth = ref(window.innerWidth)
 const toggleWidht = ref('');
+const valueSearch = ref('');
+const allNoteLowerCase = ref([]);
 
-function toglleScreen()  {
-  console.log(screenWidth.value)
+async function noteWhitoutCharacters() {
+  //Get data from database and reload notes
+  allNoteLowerCase.value = [];
+  const note = [await readAllNote()];
+  const size = note[0].length;
+
+  for (let i = 0; size > i; i++) {
+    const title = note[0][i].title;
+    const titleLower = note[0][i].title.toLowerCase(title);
+    const titleWithoutAccent = titleLower.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+    const description = note[0][i].description;
+    const descriptionLower = description.toLowerCase(description);
+    const descriptionWithoutAccent = descriptionLower.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+    const id = note[0][i].id;
+    
+    allNoteLowerCase.value.push([{ titleWithoutAccent, descriptionWithoutAccent, id }])
+  }
+  
+}
+
+const SearchNote = () => {
+  // // console.log(valueSearch.value)
+  // const arr = ['Geeks', 'gfg', 'GeeksforGeeks', 'abc'];
+  // // const textSearched = allNote.value.filter(str => str.includes(valueSearch.value));
+  // const textSearched = arr.filter(str => str.includes(valueSearch.value));
+  // console.log(textSearched);
+  
+  
+
+
+  
+
+}
+
+
+
+function toggleScreen()  {
   if(screenWidth.value < 500) {
     toggleWidht.value = true;
     console.log("largura da tela < 500", screenWidth.value)
   }else{
     //toggleWidht.value = false;
     console.log("largura da tela > 500", screenWidth.value)
-    toggleWidht.value = true;
+    toggleWidht.value = false;
   }
 }
 
@@ -50,7 +89,7 @@ const viewNote = (id) => {
       const description = allNote.value[i][0].description;
       indexNote.value = [id, title, description];
       indexNoteCopy.value = [id, title, description];
-      toglleModal.value = true;
+      toggleModal.value = true;
     }
   }
 }
@@ -80,7 +119,7 @@ async function addTitleDescription(index) {
   allNote.value.push([{ title, description, id }])
   enteredTitle.value = "";
   enteredDescription.value = "";
-  toglleTitle.value = false;
+  toggleTitle.value = false;
 }
 
 const removeNote = () => {
@@ -88,7 +127,7 @@ const removeNote = () => {
   const id = indexNote.value[0];
   deleteNote(id);
   reloadNote();
-  toglleModal.value = false;
+  toggleModal.value = false;
 }
 
 const editeNote = () => {
@@ -98,13 +137,14 @@ const editeNote = () => {
   const description = indexNote.value[2];
   setNote(id, title, description);
   reloadNote();
-  toglleModal.value = false;
+  toggleModal.value = false;
 }
 
 //load the function when page is opened
 onMounted(
   reloadNote(),
-  toglleScreen()
+  toggleScreen(),
+  noteWhitoutCharacters()
   
 );
 
@@ -112,12 +152,17 @@ onMounted(
 
 <template>
   <section class="h-screen flex flex-col w-full dark:bg-zinc-900">
-    <div class="flex justify-center ">
+    <div class="flex justify-center shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] p-2 rounded-md dark:bg-zinc-900 mb-2">
+      <input v-bind="SearchNote()" @input="event => valueSearch = event.target.value " placeholder="Pesquise suas notas"
+              class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900">
+    </div>
+    <!-- Enter note when screen is large than 500px -->
+    <div v-if="!toggleWidht" class="flex justify-center ">
       <div
         class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] p-2 rounded-md dark:bg-zinc-900 mb-2">
-        <input @click="toglleTitle = true" id="title" type="text" v-model="enteredTitle" placeholder="Título"
+        <input @click="toggleTitle = true" id="title" type="text" v-model="enteredTitle" placeholder="Título"
           class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
-        <div v-if="toglleTitle" class="">
+        <div v-if="toggleTitle" class="">
           <input id="description" type="text" v-model="enteredDescription" placeholder="Escreva uma nota"
             class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
           <button id="btnsave" @click="addTitleDescription(index)"
@@ -125,30 +170,37 @@ onMounted(
         </div>
       </div>
     </div>
+
+    <!-- view saved notes -->
+
     <div class="">
       <div class="grid xl:grid-cols-9 xl:gap-4 md:grid-cols-5 md:gap-3 ph:grid-cols-2 ph:gap-2 dark:bg-zinc-900">
         <div
           class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] p-2 rounded-md mt-2 content-start break-words font-semibold hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] dark:bg-zinc-900"
-          v-for="entered in allNote" :key="entered" @click="viewNote(entered[0].id), toglleTitle = false">{{
+          v-for="entered in allNote" :key="entered" @click="viewNote(entered[0].id), toggleTitle = false">{{
             entered[0].title
           }}
           <p class="font-normal text-center dark:bg-zinc-900">{{ entered[0].description }}</p>
         </div>
       </div>
     </div>
-    <!--Only screen < 500px-->
-    <div v-if="toggleWidht" class="items-end  place-self-end h-full bg-black inline-block align-bottom">
-      <button class="align-items"><font-awesome-icon icon="fa-solid fa-circle-plus" /></button>
-    </div>
-    <!--modal-->
+
+    <!-- button to enter note when screen is smaller than 500px -->
+    
+      <footer v-if="toggleWidht" class="sticky bottom-0 grid content-end place-self-end h-full align-bottom pb-12">
+        <button class="content-end"><font-awesome-icon icon="fa-solid fa-circle-plus" size="2xl"/>
+        </button>
+      </footer>      
+    
+    <!--modal view note -->
     <div>
       <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50 overscroll-none "
-        v-if="toglleModal">
+        v-if="toggleModal">
         <div class="relative wx-auto max-w-2xl w-96 max-h-full my-20 ">
           <div class="bg-white dark:bg-zinc-900 w-full rounded  shadow-2xl flex flex-col pt-0 pb-4 px-4 ">
             <div class="place-self-end">
               <button class="rounded-full hover:bg-gray-100 p-1 w-[30px] h-30px"
-                @click="toglleModal = false"><font-awesome-icon icon="fa-solid fa-xmark"
+                @click="toggleModal = false"><font-awesome-icon icon="fa-solid fa-xmark"
                   style="color: #707070;" /></button>
             </div>
             <input v-bind="ViewSave()" :value="indexNote[1]" @input="event => indexNote[1] = event.target.value"
@@ -161,14 +213,16 @@ onMounted(
                   @click="editeNote">Save</button>
               </span>
               <div :class="{ shake: disabled }">
-                <span class="rounded-full hover:bg-gray-100 p-1 w-[30px] m-3 mb-5 h-30px"><font-awesome-icon
-                    icon="fa-solid fa-trash" style="color: #707070;" /></span>
+                <span class="rounded-full hover:bg-gray-100 p-1 w-[30px] m-3 mb-5 h-30px">
+                  <button @click="removeNote"><font-awesome-icon
+                    icon="fa-solid fa-trash" style="color: #707070;" /></button>
+                  </span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div v-if="toglleModal" class="absolute z-40 inset-0 opacity-25 bg-black overscroll-none">
+      <div v-if="toggleModal" class="absolute z-40 inset-0 opacity-25 bg-black overscroll-none">
       </div>
     </div>
   </section>
