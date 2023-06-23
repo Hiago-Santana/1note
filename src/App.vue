8f048ddcc91e39725729e511c6e66fcc6e067372
Hiago-Santana <hiago_douglas@hotmail.com>
2023-06-22 21:37:51 -0300

    Upgrade

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@ const valueSearch = ref('');
 const allNoteLowerCase = ref([]);
 const searchResult = ref([]);
 const toggleSearch = ref(false);
+const valueSearchCopy = ref();
+const buttonEnterNote = ref(false);
 
 
 async function noteWhitoutCharacters() {
@@ -42,35 +44,40 @@ async function noteWhitoutCharacters() {
 }
 
 const searchNote = () => {
-  const content = valueSearch.value.length;
-  if (content > 0) {
-    toggleSearch.value = true;
-    const size = allNoteLowerCase.value.length;
-    const search1 = valueSearch.value;
-    const search1Lower = search1.toLowerCase(search1);
-    const search = search1Lower.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    searchResult.value = [];
-    // console.log("size", size)
-    for (let i = 0; i < size; i++) {
-
-      const title = allNoteLowerCase.value[i][0].title;
-      const description = allNoteLowerCase.value[i][0].description;
-      const titleIndex = title.indexOf(search);
-      const descriptionIndex = description.indexOf(search);
-      if (titleIndex >= 0 || descriptionIndex >= 0) {
-        const title = allNote.value[i][0].title;
-        const description = allNote.value[i][0].description;
-        const id = allNoteLowerCase.value[i][0].id;
-        searchResult.value.push([{ id, title, description }]);
-      }
-    }
-
-  } else
-    if (searchResult.value.length > 0) {
+  if (valueSearchCopy.value != valueSearch.value) {
+    const content = valueSearch.value.length;
+    if (content > 0) {
       toggleSearch.value = true;
-      console.log("content", content)
-      console.log("toggleSearch", toggleSearch.value)
-    }
+      const size = allNoteLowerCase.value.length;
+      const search1 = valueSearch.value;
+      const search1Lower = search1.toLowerCase(search1);
+      const search = search1Lower.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+      searchResult.value = [];
+      // console.log("size", size)
+
+      for (let i = 0; i < size; i++) {
+        const title = allNoteLowerCase.value[i][0].title;
+        const description = allNoteLowerCase.value[i][0].description;
+        const titleIndex = title.indexOf(search);
+        const descriptionIndex = description.indexOf(search);
+
+        if (titleIndex >= 0 || descriptionIndex >= 0) {
+          const title = allNote.value[i][0].title;
+          const description = allNote.value[i][0].description;
+          const id = allNoteLowerCase.value[i][0].id;
+          searchResult.value.push([{ id, title, description }]);
+        }
+      }
+
+    } else
+      if (searchResult.value.length > 0) {
+        toggleSearch.value = true;
+        console.log("content", content)
+        console.log("toggleSearch", toggleSearch.value)
+      }
+    valueSearchCopy.value = valueSearch.value;
+  }
+
 }
 
 const returnSearch = () => {
@@ -78,7 +85,6 @@ const returnSearch = () => {
   valueSearch.value = "";
   toggleSearch.value = false;
 }
-
 
 function toggleScreen() {
   screenWidth.value = window.innerWidth;
@@ -137,7 +143,8 @@ async function addTitleDescription(index) {
   //Add title and description to database
   const title = enteredTitle.value;
   const description = enteredDescription.value;
-  await addNote(title, description);
+  if(title !="" && description !=""){
+    await addNote(title, description);
   const note = [await readAllNote()];
   const size = note[0].length - 1;
   const id = note[0][size].id;
@@ -145,6 +152,10 @@ async function addTitleDescription(index) {
   enteredTitle.value = "";
   enteredDescription.value = "";
   toggleTitle.value = false;
+  
+  }
+  buttonEnterNote.value = false;
+  console.log(buttonEnterNote);
 }
 
 const removeNote = () => {
@@ -165,66 +176,40 @@ const editeNote = () => {
   toggleModal.value = false;
 }
 
+// const buttonEnterNote = () => {
+//   enterNote.value = true;
+// }
+
 //load the function when page is opened
 onMounted(() => {
   reloadNote(),
     noteWhitoutCharacters(),
-    window.addEventListener('resize', toggleScreen)
-    toggleScreen();    
+    window.addEventListener('resize', toggleScreen),
+    toggleScreen();
 });
 
 </script>
 
 <template>
-  <section class="h-screen flex flex-col w-full dark:bg-zinc-900">
-
-    <!-- search value -->
-    <div
-      class="flex justify-center shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] p-2 rounded-md dark:bg-zinc-900 mb-2">
-      <span v-if="toggleSearch">
-        <button @click="returnSearch"><font-awesome-icon icon="fa-solid fa-arrow-left" /></button>
-      </span>
-      <input v-bind="searchNote()" :value="valueSearch" @input="event => valueSearch = event.target.value"
-        placeholder="Pesquise suas notas"
-        class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900">
-    </div>
-
-    <!-- display search value -->
-    <div class="">
-      <div class="grid xl:grid-cols-9 xl:gap-4 md:grid-cols-5 md:gap-3 ph:grid-cols-2 ph:gap-2 dark:bg-zinc-900">
-        <div
-          class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] p-2 rounded-md mt-2 content-start break-words font-semibold hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] dark:bg-zinc-900"
-          v-for="entered in searchResult" :key="entered" @click="viewNote(entered[0].id), toggleTitle = false">{{
-            entered[0].title
-          }}
-          <p class="font-normal text-center dark:bg-zinc-900">{{ entered[0].description }}</p>
-        </div>
-      </div>
-    </div>
-
-    <!-- Enter note when screen is large than 500px -->
-    <div v-if="!toggleSearch">
-      <div v-if="!toggleWidht" class="flex justify-center ">
-        <div
-          class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] p-2 rounded-md dark:bg-zinc-900 mb-2">
-          <input @click="toggleTitle = true" id="title" type="text" v-model="enteredTitle" placeholder="Título"
-            class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
-          <div v-if="toggleTitle" class="">
-            <input id="description" type="text" v-model="enteredDescription" placeholder="Escreva uma nota"
-              class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
-            <button id="btnsave" @click="addTitleDescription(index)"
-              class="bg-blue-500 hover:bg-blue-700 text-white-950 font-bold py-2 px-4 rounded ">Salvar</button>
-          </div>
-        </div>
+  <section >
+    <div v-if="!buttonEnterNote" class="h-screen flex flex-col w-full dark:bg-zinc-900">
+      <!-- search value -->
+      <div
+        class="flex justify-center shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] p-2 rounded-md dark:bg-zinc-900 mb-2">
+        <span v-if="toggleSearch">
+          <button @click="returnSearch"><font-awesome-icon icon="fa-solid fa-arrow-left" /></button>
+        </span>
+        <input v-bind="searchNote()" :value="valueSearch" @input="event => valueSearch = event.target.value"
+          placeholder="Pesquise suas notas"
+          class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900">
       </div>
 
-      <!-- view saved notes -->
-
+      <!-- display search value -->
       <div class="">
-        <div class="grid xl:grid-cols-7 xl:gap-4 md:grid-cols-5 md:gap-3 ph:grid-cols-2 ph:gap-2 dark:bg-zinc-900">
+        <div class="grid xl:grid-cols-9 xl:gap-4 md:grid-cols-5 md:gap-3 ph:grid-cols-2 ph:gap-2 dark:bg-zinc-900">
           <div
             class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] p-2 rounded-md mt-2 content-start break-words font-semibold hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] dark:bg-zinc-900"
-            v-for="entered in allNote" :key="entered" @click="viewNote(entered[0].id), toggleTitle = false">{{
+            v-for="entered in searchResult" :key="entered" @click="viewNote(entered[0].id), toggleTitle = false">{{
               entered[0].title
             }}
             <p class="font-normal text-center dark:bg-zinc-900">{{ entered[0].description }}</p>
@@ -232,47 +217,96 @@ onMounted(() => {
         </div>
       </div>
 
-      <!-- button to enter note when screen is smaller than 500px -->
-
-      <footer v-if="toggleWidht" class="sticky bottom-0 grid justify-items-end content-end h-full pb-8">
-        <button class="content-end"><font-awesome-icon icon="fa-solid fa-circle-plus" size="2xl" />
-        </button>
-      </footer>
-
-      <!--modal view note -->
-
-    </div>
-    <div>
-      <div class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50 overscroll-none "
-        v-if="toggleModal">
-        <div class="relative wx-auto max-w-2xl w-96 max-h-full my-20 ">
-          <div class="bg-white dark:bg-zinc-900 w-full rounded  shadow-2xl flex flex-col pt-0 pb-4 px-4 ">
-            <div class="place-self-end">
-              <button class="rounded-full hover:bg-gray-100 p-1 w-[30px] h-30px"
-                @click="toggleModal = false"><font-awesome-icon icon="fa-solid fa-xmark"
-                  style="color: #707070;" /></button>
+      <!-- Enter note when screen is large than 500px -->
+      <div v-if="!toggleSearch">
+        <div v-if="!toggleWidht" class="flex justify-center ">
+          <div
+            class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] p-2 rounded-md dark:bg-zinc-900 mb-2">
+            <input @click="toggleTitle = true" id="title" type="text" v-model="enteredTitle" placeholder="Título"
+              class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
+            <div v-if="toggleTitle" class="">
+              <input id="description" type="text" v-model="enteredDescription" placeholder="Escreva uma nota"
+                class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
+              <button id="btnsave" @click="addTitleDescription(index)"
+                class="bg-blue-500 hover:bg-blue-700 text-white-950 font-bold py-2 px-4 rounded ">Salvar</button>
             </div>
-            <input v-bind="ViewSave()" :value="indexNote[1]" @input="event => indexNote[1] = event.target.value"
-              class="text-2xl font-bold focus:outline-none dark:bg-zinc-900">
-            <input v-bind="ViewSave()" :value="indexNote[2]" @input="event => indexNote[2] = event.target.value"
-              class="overflow-auto focus:outline-none dark:bg-zinc-900">
-            <div class="items-baseline place-self-end">
-              <span v-if="toggleSave" class="{ buttonSave }">
-                <button id="buttonSave" class="rounded-full hover:bg-gray-100 text-gray px-2 my-0 py-0 w-12/12 mx-3"
-                  @click="editeNote">Save</button>
-              </span>
-              <div :class="{ shake: disabled }">
-                <span class="rounded-full hover:bg-gray-100 p-1 w-[30px] m-3 mb-5 h-30px">
-                  <button @click="removeNote"><font-awesome-icon icon="fa-solid fa-trash"
-                      style="color: #707070;" /></button>
+          </div>
+        </div>
+
+        <!-- view saved notes -->
+
+        <div class="">
+          <div class="grid xl:grid-cols-7 xl:gap-4 md:grid-cols-5 md:gap-3 ph:grid-cols-2 ph:gap-2 dark:bg-zinc-900">
+            <div
+              class="container shadow-[0_7px_15px_1px_rgba(0,0,0,0.3)] p-2 rounded-md mt-2 content-start break-words font-semibold hover:shadow-[0_7px_15px_1px_rgba(0,0,0,0.5)] dark:bg-zinc-900"
+              v-for="entered in allNote" :key="entered" @click="viewNote(entered[0].id), toggleTitle = false">{{
+                entered[0].title
+              }}
+              <p class="font-normal text-center dark:bg-zinc-900">{{ entered[0].description }}</p>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <!--modal view note -->
+
+      </div>
+      <div>
+        <div
+          class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50 overscroll-none "
+          v-if="toggleModal">
+          <div class="relative wx-auto max-w-2xl w-96 max-h-full my-20 ">
+            <div class="bg-white dark:bg-zinc-900 w-full rounded  shadow-2xl flex flex-col pt-0 pb-4 px-4 ">
+              <div class="place-self-end">
+                <button class="rounded-full hover:bg-gray-100 p-1 w-[30px] h-30px"
+                  @click="toggleModal = false"><font-awesome-icon icon="fa-solid fa-xmark"
+                    style="color: #707070;" /></button>
+              </div>
+              <input v-bind="ViewSave()" :value="indexNote[1]" @input="event => indexNote[1] = event.target.value"
+                class="text-2xl font-bold focus:outline-none dark:bg-zinc-900">
+              <input v-bind="ViewSave()" :value="indexNote[2]" @input="event => indexNote[2] = event.target.value"
+                class="overflow-auto focus:outline-none dark:bg-zinc-900">
+              <div class="items-baseline place-self-end">
+                <span v-if="toggleSave" class="{ buttonSave }">
+                  <button id="buttonSave" class="rounded-full hover:bg-gray-100 text-gray px-2 my-0 py-0 w-12/12 mx-3"
+                    @click="editeNote">Save</button>
                 </span>
+                <div :class="{ shake: disabled }">
+                  <span class="rounded-full hover:bg-gray-100 p-1 w-[30px] m-3 mb-5 h-30px">
+                    <button @click="removeNote"><font-awesome-icon icon="fa-solid fa-trash"
+                        style="color: #707070;" /></button>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div v-if="toggleModal" class="absolute z-40 inset-0 opacity-70 bg-black overscroll-none">
+        </div>
       </div>
-      <div v-if="toggleModal" class="absolute z-40 inset-0 opacity-70 bg-black overscroll-none">
+      <!-- button to enter note when screen is smaller than 500px -->
+
+      <footer v-if="toggleWidht && !toggleSearch" class="sticky bottom-0 grid justify-items-end content-end h-full pb-8">
+          <button @click="buttonEnterNote = true" class="content-end"><font-awesome-icon icon="fa-solid fa-circle-plus"
+              size="2xl" />
+          </button>
+        </footer>
+    </div>
+
+    <div v-if="buttonEnterNote" class="h-screen">
+      <div>
+        <button @click="addTitleDescription(index)"><font-awesome-icon icon="fa-solid fa-arrow-left" /></button>
       </div>
+    
+        <input type="text" v-model="enteredTitle" placeholder="Título"
+        class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
+      <input type="text" v-model="enteredDescription" placeholder="Escreva uma nota"
+        class="break-words input input-bordere w-full rounded-md m-1 focus:outline-none dark:bg-zinc-900" />
+        <div class="sticky bottom-0 grid justify-items-end content-end h-full pb-8">
+        <button id="btnsave" @click="addTitleDescription(index)"
+          class="bg-blue-500 hover:bg-blue-700 text-white-950 font-bold py-2 mb-10 px-4 rounded ">Salvar</button>
+      </div> 
     </div>
   </section>
 </template>
