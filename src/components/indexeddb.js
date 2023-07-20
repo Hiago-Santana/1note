d01c84bc72ba5a69af80e05a0a7a3f4f22cc4e70
Hiago-Santana <hiago_douglas@hotmail.com>
2023-07-20 11:23:43 -0300

    Update

--- src/components/indexeddb.js
+++ src/components/indexeddb.js
@@ -9,7 +9,7 @@ export function openDataBase() {
         , {
           // The 'id' property of the object will be the key.
           keyPath: "id",
-        //   // If it isn't explicitly set, create a value by auto incrementing.
+          // If it isn't explicitly set, create a value by auto incrementing.
           autoIncrement: true
         }
         );
@@ -24,23 +24,24 @@ export function openDataBase() {
 export async function addNote(title, description) {
 
   const resultArray = Array.isArray(description)
-  console.log("resultArray",resultArray)
-  console.log("testeArray",description[0].checkBox)  
   const db = await openDataBase();
 
   if(resultArray){
-    
-    // db.add("note", {title: title, description: description[0].description});
-    db.add("note", {title: title, description: {list: description[0].description, checkBox: description[0].checkBox}});
+    db.add("note", {title: title});
     const allNote = await db.getAll("note");
-    const teste = allNote.slice(-1)[0]
-    console.log("teste",teste.id)
-
+    const note = allNote.slice(-1)[0]
+    const dbList = await openDataBaseList();
     const size = description.length
-    console.log("size",size)
-  }
-  
-    
+    for(let i = 0;i < size;i++){
+      const checkBoxList = description[i].checkBox;
+      const descriptionList = description[i].description
+      console.log("checkBox",checkBoxList)
+      console.log("descriptionList",descriptionList)
+      dbList.add("noteList",{idNote: note.id, checkBox: checkBoxList, description: descriptionList})
+    }
+  }else{
+    db.add("note", {title: title, description: description});
+  } 
   const store = db.transaction('note').store;
   const cursor = await store.openCursor();
 }
@@ -59,4 +60,25 @@ export async function deleteNote(id) {
 export async function setNote(id,title,description) {
   const db = await openDataBase();
   await db.put("note",{id: id, title: title, description: description})  
+}
+
+export function openDataBaseList() {
+  return new Promise((resolve, reject) => {
+    const db = openDB("noteListDB", 1, {
+      upgrade(db) {
+        // Create a store of objects
+        const store = db.createObjectStore("noteList"
+        , {
+          // The 'id' property of the object will be the key.
+          keyPath: "id",
+          // If it isn't explicitly set, create a value by auto incrementing.
+          autoIncrement: true
+        }
+        );
+        // Create an index on the 'note' property of the objects.
+        store.createIndex("strengthIndex", "strength");
+      },
+    });
+    resolve(db);
+  });
 }
